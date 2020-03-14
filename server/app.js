7cba2a5f1286dce7583286a11aaae327d2b97955
LisaCee <lisaceedesign@gmail.com>
2020-03-14 16:22:44 -0700

    mongoose server connected

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 // middleware
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://lisacee:baxterdog5@ds013004.mlab.com:13004/gql-ninja');
+mongoose.connection.once('open', () => {
+    console.log('Connected to DB')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
