69db0e57742af57fa8fee882967282b4b94e1aec
LisaCee <lisaceedesign@gmail.com>
2020-03-16 13:16:47 -0700

    book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div id="main">
-         <h1>Hi</h1>
+         <h1>Lisa's Book List</h1>
+         <BookList />
       </div>
     );
   }
