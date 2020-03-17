7a1c942c23912f49b8a29d2a93ab7272eda2fb6e
LisaCee <lisaceedesign@gmail.com>
2020-03-17 14:53:36 -0700

    form to add book and fill authors

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
-import {gql} from 'apollo-boost';
-import {graphql} from 'react-apollo';
-
-const getBooksQuery = gql`
-    { 
-        books {
-            name
-            id
-        }
-    }
-`;
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getBooksQuery } from "../queries/queries";
 
 class BookList extends Component {
-    displayBooks() {
-        var data = this.props.data;
-        if (data.loading) {
-            return(<div>Loading books...</div>)
-        } else {
-            return data.books.map(book => {
-                return (<li key={book.id}>{book.name}</li>)
-            })
-        }
-       
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
     }
+  }
   render() {
     return (
       <div>
-          <ul id="book-list">
-              {this.displayBooks()}
-          </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
